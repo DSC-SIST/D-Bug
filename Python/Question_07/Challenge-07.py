1aea3fc0eb4d09cbf589ee3b776314711116a9e4
Avinash Mahanthi <54079190+AvinashMahanthi@users.noreply.github.com>
2020-10-19 23:10:18 +0530

    Update Challenge-07.py

--- Python/Question_07/Challenge-07.py
+++ Python/Question_07/Challenge-07.py
@@ -1,14 +1,13 @@
-class Solution:
-    # Function to check parentheses
-    def isValid(self, s: str) -> bool:
-        # creating a ampty stack and all valid parentheses
-        stack, pchar = [], {"(": ")", "{": "}", "[": "]"}
-        #loop to check the parentheses in input string s
-        # fix the bug somewhere below this code.
-        for parenthese in s:
-            if parenthese in pchar:
-                stack.append(parenthese)
-            elif len(stack) == 0:
-                return False
+def isValid(s: str) -> bool:
+    # creating a empty stack and all valid parentheses
+    stack, pchar = [], {"(": ")", "{": "}", "[": "]"}
+    #loop to check the parentheses in input string s
+    # fix the bug somewhere below this code.
+    for parenthese in s:
+        if parenthese in pchar:
+            stack.append(parenthese)
+        elif len(stack) == 0:
+            return False
             #if the stack is empty that means the input string is a valid parenthese
-        return len(stack) == 0
+    return len(stack) == 0
+isValid("Hello world(:-)")
