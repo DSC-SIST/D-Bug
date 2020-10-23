148f4abb1b46bcd3810b8be2f2d20de3206d4a3d
Akash M <akashm1999@gmail.com>
2020-10-23 11:09:18 +0530

    Added index.html and CNAME

--- JavaScript/Question_07/question_07.js
+++
@@ -1,8 +0,0 @@
-function findLongestWordLength(str) {
-  const longestWord = str.split('').map((x) => x.length).sort((a,b) => b-a);
-  return longestWord[0];
-}
-
-console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog")); // It should return 6
-console.log(findLongestWordLength("May the force be with you")); // It should return 5
-console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology")); // It should return 19
