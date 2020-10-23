32679a649d53ad938914274cdb59f7e1622e5e09
supriyasinhaa <supriya.sinha2k@gmail.com>
2020-10-23 11:42:30 +0530

    Updated ques-20

--- Java/Question_20/Question_20.java
+++ Java/Question_20/Question_20.java
@@ -11,9 +11,12 @@ public class Main
         int i,j,lines=5; 
         for(i=1; i<=lines; i++)
         {                                                   // this loop is used to print the lines
-            for(j=lines; j>=1; j--)
+            for(j=lines; j>=1; j++)
             {                                               // this loop is used to print numbers in a line  
-                // Write the condition to print the following output.    
+                if(j ==i )
+                    System.out.print(j + i);
+                else
+                    System.out.print("*");    
             }  
             System.out.println(" ");  
         }  
