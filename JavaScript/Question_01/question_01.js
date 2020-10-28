45516b5988621a08ccf7b5513d31b1f36329ccde
Arjun Dave <davearjun25@gmail.com>
2020-10-28 23:17:03 +0530

    Create question_01.js
    I have created a new array and concatenated the 2 arrays so that the condition can be checked for both the arrays correctly without the first array having elements on both arrays.

---
+++ JavaScript/Question_01/question_01.js
@@ -0,0 +1,10 @@
+function diffArray(arr1, arr2) {
+    arr = arr1.concat(arr2)
+    return arr.filter(item => (arr1.includes(item) && !arr2.includes(item)) || (!arr1.includes(item) && arr2.includes(item)));
+    }
+    
+    // It should return [4]
+    console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+    
+    // It should return ['pink wool']
+    console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
