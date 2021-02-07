037c8a359da263fbca085dfa29d15fb55dae5a65
Kamal Sharma <43444282+KamalDGRT@users.noreply.github.com>
2021-02-07 21:12:47 +0530

    Revert "C challenges✨ (#29)"
    This reverts commit a35c9f4dc82477fe65172ceedad64a1e195c9869.

---
+++ C/Question_11/question_11.c
@@ -0,0 +1,29 @@
+/* 
+    C Program to check whether a given number is positive or negative.
+
+    Difficulty level: Easy    
+*/
+
+#include <stdio.h>
+
+int Main()
+{
+    int number;
+    printf("Enter a number:");
+    scanf("%d", number)
+
+    if (number <= 0)
+    {
+        if (Number == 0)
+            printf("The given number is 0!");
+        else
+        {
+            print("The given number is Negative..!!");
+        }
+    }
+    else
+    {
+        print("The given number is Positive..!!")
+    }
+    return 0;
+}
---
+++ C/Question_14/README.md
@@ -0,0 +1,15 @@
+# Question 14
+
+### C program to calculate the Area of the square.
+
+## Difficulty Level
+
+Easy
+
+## Sample Input
+
+      Enter the Length of Side : 7
+
+## Sample Output
+
+      Area of Square : 49
